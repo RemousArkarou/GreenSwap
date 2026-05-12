13cb5b1563ce5a34e63fc07e0c94e14329d68233
ANKERBOYGame <daanvanyperen@ziggo.nl>
2026-05-12 15:31:31 +0200

    begining inlog page

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using GreenSwap.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -11,8 +6,14 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using GreenSwap.Pages.Login;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,12 +28,12 @@ namespace GreenSwap
         public MainWindow()
         {
             InitializeComponent();
+            //using var db = new AppDbContext();
 
-            using (var db = new AppDbContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-            }
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+
+            contentFrame.Navigate(typeof(LoginPage));
         }
     }
 }
